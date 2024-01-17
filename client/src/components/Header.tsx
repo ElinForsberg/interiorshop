35bcf67f5d15d81cfa901ead78fcbaf5c10bee6c
Elin Forsberg <elinfsp96@hotmail.com>
2024-01-17 19:48:16 +0100

    s@emotion/styled added to project

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,27 @@
 
 import styled from '@emotion/styled';
 import ShoppingCart from './ShoppingCart';
+import { Link } from 'react-router-dom';
+import { Box, Grid } from '@mui/material';
 
 function Header() {
   return (
-    <HeaderContainer>
-        <ShoppingCart/>
-    </HeaderContainer>
+    <Box>
+        <HeaderContainer container direction="row">
+            <Grid item xs={10}><Link to={'/products'}>InteriorShop</Link></Grid>     
+            <Grid item xs={1}><Link to={'/login'}>Logga in</Link></Grid>    
+            <Grid item xs={1}><ShoppingCart/></Grid>    
+        </HeaderContainer>
+    </Box>
+    
+    
   )
 }
-const HeaderContainer = styled('div')`
-  background-color: aquamarine;
+const HeaderContainer = styled(Grid)`
+  background-color: lightgrey;
   height: 100px;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 
