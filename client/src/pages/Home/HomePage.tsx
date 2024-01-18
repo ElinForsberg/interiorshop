7b34b939497cc629ea5e7ccdce1963e80593ee6f
Elin Forsberg <elinfsp96@hotmail.com>
2024-01-18 08:38:59 +0100

    filter function for products

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -1,11 +1,12 @@
 
-import { Button } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import Header from '../../components/Header';
 import {  useGetProductsQuery, useGetQuantityInStockQuery } from '../../redux/services/productsApi'
 import ProductCard from './ProductCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { clearCart } from '../../redux/slices/shoppingCartSlice';
+import { useEffect, useState } from 'react';
 
 
 
@@ -17,9 +18,22 @@ function HomePage() {
     const dispatch = useDispatch();
     const shoppingCart = useSelector((state: RootState) => state.shoppingCart);
 
-  console.log("stripe", productsData);
-    console.log("db", quantityData);
+    const [selectedCategory, setSelectedCategory] = useState<string | undefined>(undefined);
+    const [categories, setCategories] = useState<string[]>([]);
+    // const categories: string[] = Array.from(new Set(productsData?.data.map((product) => product.metadata.category || '')));
 
+    useEffect(() => {
+      if (productsData?.data) {
+        const uniqueCategories = Array.from(new Set(productsData.data.map((product) => product.metadata.category || '')));
+        setCategories(uniqueCategories);
+      }
+    }, [productsData]);
+    
+    const filteredProducts = productsData?.data.filter(
+      (product) => !selectedCategory || product.metadata.category === selectedCategory
+    );
+
+    
     const seeCart = () => {
         console.log(shoppingCart);
         
@@ -27,6 +41,8 @@ function HomePage() {
     const emptyCart = () => {
         dispatch(clearCart());
     }
+
+  
     
   if (productsIsLoading || quantityIsLoading) {
     return <div>Loading...</div>;
@@ -40,13 +56,31 @@ function HomePage() {
     <>
     <Header/>
     <div>
-    {productsData?.data.map((product) => {
-      // Find corresponding quantity data for the current product
-      const quantity = quantityData?.find((q) => q.stripeId === product.id);
-      const inStock = quantity?.inStock || 0; // Default to 0 if quantity is undefined
-      return <ProductCard key={product.id} stripeProduct={product} productInStock={inStock} />;
-    })}
-  </div>
+        <FormControl fullWidth>
+          <InputLabel id="category-filter-label">Kategorier</InputLabel>
+          <Select
+            labelId="category-filter-label"
+            id="category-filter"
+            value={selectedCategory}
+            label="Category Filter"
+            onChange={(e) => setSelectedCategory(e.target.value as string)}
+          >
+            <MenuItem value="">Visa alla</MenuItem>
+            {categories.map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        {filteredProducts?.map((product) => {
+          const quantity = quantityData?.find((q) => q.stripeId === product.id);
+          const inStock = quantity?.inStock || 0;
+          return <ProductCard key={product.id} stripeProduct={product} productInStock={inStock} />;
+        })}
+      </div>
   <Button onClick={seeCart}> Se varukorgen</Button>
   <Button onClick= {emptyCart}>Töm varukorgen</Button>
   </>
