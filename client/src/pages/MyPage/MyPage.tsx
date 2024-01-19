2a6360df792d4969d5ddd29e6209756365665e79
Elin Forsberg <elinfsp96@hotmail.com>
2024-01-19 17:50:31 +0100

    mypage adjustment

--- client/src/pages/MyPage/MyPage.tsx
+++ client/src/pages/MyPage/MyPage.tsx
@@ -26,44 +26,53 @@ function MyPage() {
 
     
     
-  return (
-    <>
-    <Header/>
-    <div>Mina Sidor</div>
-    <p>Dina ordrar</p>
-    {orderData && orderData.map((order: Order) => (
-        <OrderContainer key={order._id}>
-          {/* Render order details here */}
-          <p>Order ID: {order._id}</p>
-          <p>name: {order.name}</p>
-          <p>email: {order.email}</p>
-          <p>Leverans adress: {order.address.street}, {order.address.postal_code}, {order.address.city}, {order.address.country}</p>
-           <p>Dina köp</p>
-           {order.products && order.products.map(product => (
-            <div key={product.stripeId}>
-              {/* Render product details here */}
-              <p>{product.description}</p>
-              <p>Antal: {product.quantity}</p>
-              <p>Pris: {product.price} {product.currency}</p>
-              <p>Summa: {product.total} {product.currency}</p>
-              {/* Add other product details as needed */}
-            </div>
-          ))}
-          <p>total summa: {order.totalSum} sek</p>
-          {order.isShipped ?
-          <p>din order är skickad</p> : 
-          <p>din order väntar på att bli skickad</p>} 
-        </OrderContainer>
-      ))}
+    return (
+        <>
+          <Header />
+          <div>Mina Sidor</div>
+          <p>Dina ordrar</p>
+          {Array.isArray(orderData) && orderData.length > 0 ? (
+            orderData.map((order: Order) => (
+              <OrderContainer key={order._id}>
+                {/* Render order details here */}
+                <p>Order ID: {order._id}</p>
+                <p>name: {order.name}</p>
+                <p>email: {order.email}</p>
+                <p>Leverans adress: {order.address.street}, {order.address.postal_code}, {order.address.city}, {order.address.country}</p>
+                <p>Dina köp</p>
+                {order.products && order.products.map(product => (
+                  <div key={product.stripeId}>
+                    {/* Render product details here */}
+                    <p>{product.description}</p>
+                    <p>Antal: {product.quantity}</p>
+                    <p>Pris: {product.price} {product.currency}</p>
+                    <p>Summa: {product.total} {product.currency}</p>
+                    {/* Add other product details as needed */}
+                  </div>
+                ))}
+                <p>total summa: {order.totalSum} sek</p>
+                {order.isShipped ?
+                  <p>din order är skickad</p> :
+                  <p>din order väntar på att bli skickad</p>}
+              </OrderContainer>
+            ))
+          ) : (
+            <NoOrdersContainer>
+              <p>Du har inga ordrar ännu.</p>
+      
+            </NoOrdersContainer>
+          )}
+        </>
+      );
+    }
     
-    </>
+    const OrderContainer = styled.div`
+      background-color: lightblue;
+    `;
     
-  )
-}
-
-const OrderContainer = styled.div`
-background-color: lightblue;
-
-`
-
-export default MyPage
+    const NoOrdersContainer = styled.div`
+      text-align: center;
+      margin-top: 20px;
+    `;
+    
+    export default MyPage;
