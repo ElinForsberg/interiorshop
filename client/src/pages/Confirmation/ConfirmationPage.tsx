bf2d22a3be0e7fcb2a40552af4baadd6de2404a6
Elin Forsberg <elinfsp96@hotmail.com>
2024-01-23 12:43:05 +0100

    confirmation page layout

--- client/src/pages/Confirmation/ConfirmationPage.tsx
+++ client/src/pages/Confirmation/ConfirmationPage.tsx
@@ -3,23 +3,34 @@ import { useVerifyPaymentMutation } from '../../redux/services/checkoutApi';
 import { useDispatch } from 'react-redux';
 import { clearCart } from '../../redux/slices/shoppingCartSlice';
 import Header from '../../components/Header';
+import { Order } from '../../redux/services/ordersApi';
+import { Typography } from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import styled from '@emotion/styled';
 
 function ConfirmationPage() {
     const [verifyPayment] = useVerifyPaymentMutation();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [isPaymentVerified, setIsPaymentVerified] = useState(false);
+   const [orderDetails, setOrderDetails] = useState<Order | null>();
     const dispatch= useDispatch();
     const sessionId = localStorage.getItem("session-id")
     
     const handleVerifyPayment = async () => {
         try {
           const result = await verifyPayment({ sessionId: sessionId || '' });
-            
+            console.log("respons", result);
           if ('data' in result) {
             setIsPaymentVerified(true);
+            
             localStorage.removeItem('session-id');
             dispatch(clearCart());
             console.log("payment is verified");
+            const orderResponse = result.data.order
+            setOrderDetails(orderResponse)
+            console.log("orderResponse", orderResponse);
+            console.log("orderDetails", orderDetails);
+            
             
           } else {
             setIsPaymentVerified(false);
@@ -38,7 +49,16 @@ function ConfirmationPage() {
     isPaymentVerified ?
   <div>
     <Header/>
-    <div>Tack för din order</div>
+    <OrderWrapper>
+    <OrderContainer>
+     <Typography>{orderDetails?.name},</Typography> 
+     <Typography>Tack för din order </Typography>
+     <Typography>ordernummer: {orderDetails?._id}</Typography>
+     <Typography>Vi skickar dina varor så fort vi kan</Typography>
+      <FavoriteBorderIcon/>
+    </OrderContainer>
+    </OrderWrapper>
+    
    </div> :
    <div>
    Din order gick inte igenom
@@ -46,4 +66,23 @@ function ConfirmationPage() {
   )
 }
 
+const OrderWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 100vw;
+height: 100vh;
+`
+
+const OrderContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 60vw;
+height: 60vh;
+border-radius: 10px;
+box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+`
 export default ConfirmationPage
